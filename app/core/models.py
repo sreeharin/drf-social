fd074f24ffb468f0f5e6c33001112fa9a0dca56f
sreeharin <sreeharinagesh@gmail.com>
2023-05-08 20:51:53 +0530

    Tests for profile model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    '''
+    Profile model is an extension of `User` model provided by Django.
+    It's automatically created when a new user is registered.
+    '''
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='profile')
+    follows = models.ManyToManyField('self', symmetrical=False, blank=True)
+
+
+@receiver(models.signals.post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile(instance, created, **kwargs) -> None:
+    '''Create a profile when a new user is registered'''
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,21 +12,55 @@ Tests which are done to the models include
 '''
 
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from core.models import Profile
+
+def create_user(**kwargs):
+    '''Helper function for creating new user'''
+    user_details = {
+        'username': 'test',
+        'password': 'testpass123',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'test@example.com',
+        }
+    user_details.update(kwargs)
+    return get_user_model().objects.create(**user_details)
 
 
 class ProfileModelTests(TestCase):
     '''Tests for profile model'''
     def test_create_profile(self) -> None:
         '''Test for creating a profile'''
+        user = create_user()
+        profiles = Profile.objects.count()
+        self.assertEqual(profiles, 1)
+        profile_exists = Profile.objects.filter(user=user).exists()
+        self.assertTrue(profile_exists)
 
     def test_get_profile(self) -> None:
-        '''Test for showing details of a profile'''
+        '''Test for getting details of a profile'''
+        user = create_user()
+        profile = user.profile
+        self.assertEqual(user.first_name, profile.user.first_name)
+        self.assertEqual(user.last_name, profile.user.last_name)
+        self.assertEqual(user.email, profile.user.email)
 
     def test_follow_profile(self) -> None:
         '''Test for following a profile'''
+        user_1 = create_user()
+        user_2 = create_user(username='test2')
+        user_1.profile.follows.add(user_2.profile)
+        self.assertIn(user_2.profile, user_1.profile.follows.all())
 
     def test_unfollow_profile(self) -> None:
         '''Test for unfollowing a profile'''
+        user_1 = create_user()
+        user_2 = create_user(username='test2')
+        user_1.profile.follows.add(user_2.profile)
+        self.assertIn(user_2.profile, user_1.profile.follows.all())
+        user_1.profile.follows.remove(user_2.profile)
+        self.assertNotIn(user_2.profile, user_1.profile.follows.all())
 
 
 class PostModelTests(TestCase):
