946c2ee7f8edd368ffd73d93d76b35d861d39ff8
sreeharin <sreeharinagesh@gmail.com>
2023-05-09 06:46:55 +0530

    model tests passing.

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,25 @@ class Profile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
         related_name='profile')
-    follows = models.ManyToManyField('self', symmetrical=False, blank=True)
+    follows = models.ManyToManyField(
+        'self', symmetrical=False,
+        blank=True, related_name='following')
+
+
+class Post(models.Model):
+    '''Model for post'''
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='posts')
+    post = models.CharField(max_length=512)
+    likes = models.ManyToManyField(Profile, related_name='likes')
+    dislikes = models.ManyToManyField(Profile, related_name='dislikes')
+
+
+class Comment(models.Model):
+    '''Model for comment'''
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=128)
 
 
 @receiver(models.signals.post_save, sender=settings.AUTH_USER_MODEL)
@@ -20,3 +38,18 @@ def create_profile(instance, created, **kwargs) -> None:
     if created:
         profile = Profile(user=instance)
         profile.save()
+
+@receiver(models.signals.m2m_changed, sender=Post.likes.through)
+@receiver(models.signals.m2m_changed, sender=Post.dislikes.through)
+def validate_like_dislike(sender, instance, action, pk_set, **kwargs) -> None:
+    '''Function for validating likes and dislikes of a post'''
+    if action == 'pre_add':
+        '''pre_add: Sent before one or more objects are added to the relation.'''
+        if sender == Post.dislikes.through and \
+            instance.likes.filter(pk__in=pk_set).exists():
+            '''Like is changed to dislike'''
+            instance.likes.remove(*pk_set)
+        elif sender == Post.likes.through and \
+            instance.dislikes.filter(pk__in=pk_set).exists():
+            '''Dislike is changed to like'''
+            instance.dislikes.remove(*pk_set)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,12 +10,18 @@ Tests which are done to the models include
     c. Like/Unlike post
     d. Comment on post
 '''
-
+from typing import Type
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from core.models import Profile
+from django.contrib.auth.models import AbstractBaseUser
+from core.models import (
+    Profile,
+    Post,
+    Comment
+)
 
-def create_user(**kwargs):
+
+def create_user(**kwargs) -> Type[AbstractBaseUser]:
     '''Helper function for creating new user'''
     user_details = {
         'username': 'test',
@@ -26,6 +32,16 @@ def create_user(**kwargs):
         }
     user_details.update(kwargs)
     return get_user_model().objects.create(**user_details)
+
+
+def create_post(user=None) -> Post:
+    '''Helper function for creating a post'''
+    if user is None:
+        user = create_user()
+    return Post.objects.create(
+        profile=user.profile,
+        post='Sample post',
+    )
 
 
 class ProfileModelTests(TestCase):
@@ -62,26 +78,84 @@ class ProfileModelTests(TestCase):
         user_1.profile.follows.remove(user_2.profile)
         self.assertNotIn(user_2.profile, user_1.profile.follows.all())
 
+    def test_following_profile(self) -> None:
+        '''Test for reverse looking up `Profile.follows`'''
+        user_1 = create_user()
+        user_2 = create_user(username='test2')
+        user_1.profile.follows.add(user_2.profile)
+        self.assertIn(user_1.profile, user_2.profile.following.all())
+
 
 class PostModelTests(TestCase):
     '''Tests for post model'''
     def test_create_post(self) -> None:
         '''Test for creating a post'''
+        user = create_user()
+        post = create_post(user)
+        posts = user.profile.posts.count()
+        self.assertEqual(posts, 1)
 
     def test_delete_post(self) -> None:
         '''Test for deleting a post'''
+        user = create_user()
+        post = create_post(user)
+        posts = user.profile.posts.count()
+        self.assertEqual(posts, 1)
+        post.delete()
+        posts = user.profile.posts.count()
+        self.assertEqual(posts, 0)
 
     def test_like_post(self) -> None:
         '''Test for liking a post'''
+        user_1 = create_user(username='user1')
+        user_2 = create_user(username='user2')
+        post = create_post()
+        post.likes.add(user_1.profile)
+        post.likes.add(user_2.profile)
+        self.assertEqual(post.likes.count(), 2)
+        self.assertIn(user_1.profile, post.likes.all())
+        self.assertIn(user_2.profile, post.likes.all())
 
     def test_unlike_post(self) -> None:
         '''Test for unliking a post'''
+        user_1 = create_user(username='user1')
+        user_2 = create_user(username='user2')
+        post = create_post()
+        post.dislikes.add(user_1.profile)
+        post.dislikes.add(user_2.profile)
+        self.assertEqual(post.dislikes.count(), 2)
+        self.assertIn(user_1.profile, post.dislikes.all())
+        self.assertIn(user_2.profile, post.dislikes.all())
 
     def test_like_unlike(self) -> None:
         '''Test for liking then unliking a post'''
+        user_1 = create_user(username='user')
+        post = create_post()
+        post.likes.add(user_1.profile)
+        self.assertEqual(post.likes.count(), 1)
+        post.dislikes.add(user_1.profile)
+        self.assertEqual(post.dislikes.count(), 1)
+        self.assertEqual(post.likes.count(), 0)
 
     def test_unlike_like(self) -> None:
         '''Test for unliking the liking a post'''
+        user_1 = create_user(username='user1')
+        post = create_post()
+        post.dislikes.add(user_1.profile)
+        self.assertEqual(post.dislikes.count(), 1)
+        post.likes.add(user_1.profile)
+        self.assertEqual(post.likes.count(), 1)
+        self.assertEqual(post.dislikes.count(), 0)
 
     def test_comment_post(self) -> None:
         '''Test for commenting a post'''
+        user_1 = create_user(username='user1')
+        post = create_post()
+        comment = Comment.objects.create(
+            profile=user_1.profile,
+            post=post,
+            comment='Test Comment',
+        )
+        self.assertEqual(post.comments.count(), 1)
+        self.assertIn(comment, post.comments.all())
+        self.assertEqual(user_1.username, comment.profile.user.username)
